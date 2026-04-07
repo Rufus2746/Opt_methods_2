6d78d2c551b0f548f0c1bc806c322efd205cfa0c
Nikita Palii <rufus2746@gmail.com>
2026-04-07 09:56:17 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,232 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import one_dim_methods as odm
+from math import acos
+from math import exp
+
+
+
+func_counter = 0
+
+
+def reset_counter():
+    global func_counter
+    func_counter = 0
+
+
+def quadratic_function(x, y):
+    global func_counter
+    func_counter += 1
+    return 100 * (y-x) ** 2 + (1-x) ** 2
+
+
+def rosenbrock_function(x, y):
+    global func_counter
+    func_counter += 1
+    return 100 * (y - x ** 2) ** 2 + (1-x) ** 2
+
+
+def personal_function(x, y):
+    global func_counter
+    func_counter += 1
+    return -(np.exp(-((x-3) / 1 ) ** 2 - ((y-1) / 3) ** 2) + 2 * np.exp(-((x-2) / 2) ** 2 - ((y-2) / 1) ** 2))
+
+
+def gradient(x, y, function):
+    grad = np.zeros(2)
+    grad[0] = odm.partial_derivative_x(x, y, function)
+    grad[1] = odm.partial_derivative_y(x, y, function)
+    return grad
+
+
+
+def find_lamda(x, y, function, one_dim_method):
+    lamda_interval = odm.lamda_interval_search(x, y, function)
+    lamda = one_dim_method(x, y, function,
+                           a=lamda_interval[0], b=lamda_interval[1])
+    return lamda
+
+
+def gradient_decent(function, one_dim_method=odm.golden_section_method, eps=1e-4):
+    coords_prev = np.zeros(2)
+    coords_next = np.zeros(2)
+    delta_func = 1.0
+    
+    #coords_prev[0] = input("Enter x0 ")
+    #coords_prev[1] = input("Enter y0 ")
+
+    coords_prev[0] = 6.
+    coords_prev[1] = 6.
+
+    x_values = np.array(coords_prev[0])
+    y_values = np.array(coords_prev[1])
+    
+    i = 0
+    print("TABLE 2 FOR GRADIENT DECENT")
+    print(f'iter num | '
+          f'{"coords":^32} | '
+          f'{"func_value":^15} | '
+          f'{"direction":^32} | '
+          f'{"lamda":^15} | '
+          f'{"delta coords":^32} | '
+          f'{"delta func":^15} | '
+          f'{"angle":^15} | '
+          f'{"gradient":^32} | ')
+    
+    while abs(delta_func) > eps:
+        direction = -gradient(coords_prev[0], coords_prev[1], function)
+        lamda = find_lamda(coords_prev[0], coords_prev[1], 
+                           function, one_dim_method)
+        
+        coords_next = coords_prev + lamda * direction
+
+        func_value_next = function(coords_next[0], coords_next[1])
+        func_value_prev = function(coords_prev[0], coords_prev[1])
+        
+        delta_coords = coords_next - coords_prev
+        delta_func = func_value_next - func_value_prev
+        
+        grad_next = gradient(coords_next[0], coords_next[1], function)
+        
+        angle = acos(np.dot(coords_next, direction) /
+                     np.linalg.norm(coords_next) / np.linalg.norm(direction))
+        
+        x_values = np.append(x_values, [coords_next[0]])
+        y_values = np.append(y_values, [coords_next[1]])
+        
+        i += 1
+        print(f'{i:^8} | '
+              f'{coords_next[0]:^15.8e}  {coords_next[1]:^15.8e} | '
+              f'{func_value_next:^15.8e} | '
+              f'{direction[0]:^15.8e}  {direction[1]:^15.8e} | '
+              f'{lamda:^15.8e} | '
+              f'{delta_coords[0]:^15.8e}  {delta_coords[1]:^15.8e} | '
+              f'{delta_func:^15.8e} | '
+              f'{angle:^15.8e} | '
+              f'{grad_next[0]:^15.8e}  {grad_next[1]:^15.8e} | ')
+        
+        coords_prev = coords_next.copy()
+
+    print("\nINFO FOR TABLE1\n")
+    print(f'eps   | iter_count | func_counter | {"coords":^30} | func value  ')
+    print(f'{eps} | {i:^10} | {func_counter:^12} | {coords_next[0]:.8e}  '
+          f'{coords_next[1]:.8e} | {func_value_next:.8e}')
+    
+    reset_counter()
+    return x_values, y_values
+
+
+def find_Hesse(delta_coords, delta_grad, Hesse):
+    I = np.eye(2)
+    coeff = 1.0 / (np.dot(delta_grad, delta_coords))
+    A1 = I - coeff * np.outer(delta_coords, delta_grad)
+    A2 = I - coeff * np.outer(delta_grad, delta_coords)
+    Hesse = np.dot(A1, np.dot(Hesse, A2)) + coeff * np.outer(delta_coords,delta_coords)
+    return Hesse
+
+
+def broyden_method(function, one_dim_method=odm.golden_section_method, eps=1e-4):
+    coords_prev = np.zeros(2)
+    coords_next = np.zeros(2)
+    delta_func = 1.0
+    
+    I = np.eye(2)
+    Hesse = I
+    
+    #coords_prev[0] = input("Enter x0 ")
+    #coords_prev[1] = input("Enter y0 ")
+
+    coords_prev[0] = 6.
+    coords_prev[1] = 6.
+
+    x_values = np.array(coords_prev[0])
+    y_values = np.array(coords_prev[1])
+
+    i = 0
+    print("TABLE 2 FOR BROYDEN METHOD")
+    print(f'{"Hesse":^25} | '
+          f'iter num | '
+          f'{"coords":^32} | '
+          f'{"func_value":^15} | '
+          f'{"direction":^32} | '
+          f'{"lamda":^15} | '
+          f'{"delta coords":^32} | '
+          f'{"delta func":^15} | '
+          f'{"angle":^15} | '
+          f'{"gradient":^32} | ')
+    
+    while abs(delta_func) > eps:
+        grad_prev = gradient(coords_prev[0], coords_prev[1], function)
+        direction = -Hesse.dot(grad_prev)  
+        lamda = find_lamda(coords_prev[0], coords_prev[1], 
+                           function, one_dim_method)
+        
+        coords_next = coords_prev + lamda * direction
+        grad_next = gradient(coords_next[0], coords_next[1], function)
+        
+        delta_coords = coords_next - coords_prev
+        delta_grad = grad_next - grad_prev
+
+        Hesse = find_Hesse(delta_coords, delta_grad, Hesse)
+        
+        func_value_next = function(coords_next[0], coords_next[1])
+        func_value_prev = function(coords_prev[0], coords_prev[1])
+        
+        delta_func = func_value_next - func_value_prev
+
+        angle = acos(np.dot(coords_next, direction) /
+                     np.linalg.norm(coords_next) / np.linalg.norm(direction))
+        
+        x_values = np.append(x_values, [coords_next[0]])
+        y_values = np.append(y_values, [coords_next[1]])
+        
+        i += 1
+        print(f'{Hesse:} | '
+              f'{i:^8} | '
+              f'{coords_next[0]:^15.8e}  {coords_next[1]:^15.8e} | '
+              f'{func_value_next:^15.8e} | '
+              f'{direction[0]:^15.8e}  {direction[1]:^15.8e} | '
+              f'{lamda:^15.8e} | '
+              f'{delta_coords[0]:^15.8e}  {delta_coords[1]:^15.8e} | '
+              f'{delta_func:^15.8e} | '
+              f'{angle:^15.8e} | '
+              f'{grad_next[0]:^15.8e}  {grad_next[1]:^15.8e} | ')
+        
+        coords_prev = coords_next.copy()
+
+    print("\nINFO FOR TABLE1\n")
+    print(f'eps   | iter_count | func_counter | {"coords":^30} | func value  ')
+    print(f'{eps} | {i:^10} | {func_counter:^12} | {coords_next[0]:.8e}  '
+          f'{coords_next[1]:.8e} | {func_value_next:.8e}')
+    
+    reset_counter()
+    return x_values, y_values
+
+
+def plot(function):
+    x = np.linspace(-10, 10, 100)
+    y = x
+    
+    xgrid, ygrid = np.meshgrid(x, y)
+    zgrid = function(xgrid, ygrid)
+    
+    plt.figure(figsize=(5, 5))
+    plt.contourf(xgrid, ygrid, zgrid, np.linspace(0, 2000, 2000))
+    #plt.contourf(xgrid, ygrid, zgrid, np.linspace(-3, 2, 50))
+    plt.colorbar()
+
+    x_values_broyden, y_values_broyden = broyden_method(function)
+    x_values_grad, y_values_grad = gradient_decent(function)
+    
+    plt.plot(x_values_broyden, y_values_broyden, 'x--r', label='broyden')
+    plt.plot(x_values_grad, y_values_grad, 'x--g', label='gradient decent')
+    
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    plot(quadratic_function)
+    #plot(rosenbrock_function)
+    # 0 2000 2000
+    #plot(personal_function)
+    # -3 2 50
---
+++ one_dim_methods.py
@@ -0,0 +1,229 @@
+import numpy as np
+from math import sqrt
+from math import log
+
+def partial_derivative_x(x, y, function, delta_x=1e-9):
+    return (function(x + delta_x, y) - function(x, y)) / delta_x
+
+def partial_derivative_y(x, y, function, delta_y=1e-9):
+    return (function(x, y + delta_y) - function(x, y)) / delta_y
+
+def fibonacci_num(n):
+    return (((1 + sqrt(5)) / 2) ** n - ((1 - sqrt(5)) / 2) ** n) / sqrt(5)
+
+def find_n_for_fibonacci(*, a=-2, b=20, eps):
+    t1 = log((1 + sqrt(5)) / 2)
+    t2 = log(sqrt(5) * (b - a) * 4 / eps / ((1+sqrt(5)) ** 2))
+    n = int(t2 / t1 + 1)
+    return n
+
+def lamda_interval_search(x, y, function):
+    sigma = 0.0001
+    lamda_prev = -1
+    k = 1
+    
+    x_der = partial_derivative_x(x, y, function)
+    y_der = partial_derivative_y(x, y, function)
+    
+    x_prev = x - lamda_prev * x_der
+    y_prev = y - lamda_prev * y_der
+    
+    x_next = x - (lamda_prev + sigma) * x_der
+    y_next = y - (lamda_prev + sigma) * y_der
+    
+    if function(x_prev, y_prev) > function(x_next, y_next):
+        lamda_next = lamda_prev + sigma
+        h = sigma
+    else:
+        lamda_next = lamda_prev - sigma
+        h = -sigma
+
+    flag = True
+    
+    while flag:
+        h *= 2
+        lamda_next += h
+        
+        x_prev, y_prev = x_next, y_next
+        
+        x_next = x - lamda_next * x_der
+        y_next = y - lamda_next * y_der
+
+        k += 1
+        if function(x_prev, y_prev) > function(x_next, y_next):
+            lamda_prev = lamda_next
+        else:
+            flag = False
+
+    if lamda_prev < lamda_next:
+        return np.array([lamda_prev-h/2, lamda_next])
+    else:
+        return np.array([lamda_next, lamda_prev+h/2]) 
+      
+def dichotomy(x, y, function, a=-2, b=20, eps=1e-7):
+    a1 = a
+    b1 = b
+    
+    x_der = partial_derivative_x(x, y, function)
+    y_der = partial_derivative_y(x, y, function)
+    
+    i = 1
+    while b1 - a1 > eps:
+        delta = eps/10
+
+        lamda1 = (a1 + b1 - delta) / 2
+        lamda2 = (a1 + b1 + delta) / 2
+        
+        x1 = x - lamda1 * x_der
+        y1 = y - lamda1 * y_der
+        
+        x2 = x - lamda2 * x_der
+        y2 = y - lamda2 * y_der
+
+        if function(x1, y1) < function(x2, y2):
+            b1 = lamda2
+        else:
+            a1 = lamda1
+            
+        i += 1
+    return (a1 + b1) / 2
+
+def parabola_method(x, y, function, a=-2, b=20, eps=1e-7,  h=0.5):
+    lamda_middle = (a + b) / 2
+    lamda_min = - lamda_middle
+    
+    x_der = partial_derivative_x(x, y, function)
+    y_der = partial_derivative_y(x, y, function)
+
+    while abs(lamda_min - lamda_middle) > eps:
+        lamda_left = lamda_middle - h
+        lamda_right = lamda_middle + h
+        
+        x_left = x - lamda_left * x_der
+        y_left = y - lamda_left * y_der
+        
+        x_right = x - lamda_right * x_der
+        y_right = y - lamda_right * y_der
+        
+        x_middle = x - lamda_middle * x_der
+        y_middle = y - lamda_middle * y_der
+        
+        lamda_min = 0.5 * ((function(x_left, y_left) * 
+                            (lamda_right + lamda_middle) - 2 * 
+                            function(x_middle, y_middle) * 
+                            (lamda_right + lamda_left) + 
+                            function(x_right, y_right) * 
+                            (lamda_middle + lamda_left)) /
+                           (function(x_left, y_left) - 2 * 
+                            function(x_middle, y_middle) + 
+                            function(x_right, y_right)))
+        
+        h = abs(lamda_min - lamda_middle)
+        lamda_middle = lamda_min
+    return lamda_min
+
+def golden_section_method(x, y, function, a=-2, b=20, eps=1e-7):
+    a1 = a
+    b1 = b
+
+    coeff_1 = (3 - sqrt(5)) / 2
+    coeff_2 = (sqrt(5) - 1) / 2
+
+    lamda1 = a + coeff_1 * (b - a)
+    lamda2 = a + coeff_2 * (b - a)
+
+    x_der = partial_derivative_x(x, y, function)
+    y_der = partial_derivative_y(x, y, function)
+
+    x1 = x - lamda1 * x_der
+    y1 = y - lamda1 * y_der
+    
+    x2 = x - lamda2 * x_der
+    y2 = y - lamda2 * y_der
+    
+    func_value_1 = function(x1, y1)
+    func_value_2 = function(x2, y2)
+    
+    i = 0
+    while b1 - a1 > eps:
+        a2 = a1
+        b2 = b1
+        
+        if func_value_1 < func_value_2:
+            b2 = lamda2
+            lamda2 = lamda1
+            x2 = x1
+            y2 = y1
+            func_value_2 = func_value_1
+            lamda1 = a2 + coeff_1 * (b2 - a2)
+            x1 = x - lamda1 * x_der
+            y1 = y - lamda1 * y_der
+            func_value_1 = function(x1, y1)
+        else:
+            a2 = lamda1
+            lamda1 = lamda2
+            x1 = x2
+            y1 = y2
+            func_value_1 = func_value_2
+            lamda2 = a2 + coeff_2 * (b2 - a2)
+            x2 = x - lamda2 * x_der
+            y2 = y - lamda2 * y_der
+            func_value_2 = function(x2, y2)
+            
+        b1 = b2
+        a1 = a2
+        i += 1
+    return (a1 + b1) / 2
+
+def fibonacci_method(x, y, function, a=-2, b=20, eps=1e-7):
+    initial_length = b - a
+    a1 = a
+    b1 = b
+
+    n = find_n_for_fibonacci(eps=1e-7)
+
+    Fn2 = fibonacci_num(n + 2)
+
+    lamda1 = a + fibonacci_num(n) / Fn2 * initial_length
+    lamda2 = a + fibonacci_num(n + 1) / Fn2 * initial_length
+    
+    x_der = partial_derivative_x(x, y, function)
+    y_der = partial_derivative_y(x, y, function)
+
+    x1 = x - lamda1 * x_der
+    y1 = y - lamda1 * y_der
+    
+    x2 = x - lamda2 * x_der
+    y2 = y - lamda2 * y_der
+    
+    func_value_1 = function(x1, y1)
+    func_value_2 = function(x2, y2)
+
+    i = 1
+    while i <= n:
+        b2 = b1
+        a2 = a1
+        
+        if func_value_1 < func_value_2:
+            b2 = lamda2
+            lamda2 = lamda1
+            x2 = x1
+            y2 = y1
+            func_value_2 = func_value_1
+            lamda1 = a2 + fibonacci_num(n - i) / Fn2 * initial_length
+            x1 = x - lamda1 * x_der
+            y1 = y - lamda1 * y_der
+            func_value_1 = function(x1, y1)
+        else:
+            a2 = lamda1
+            lamda1 = lamda2
+            func_value_1 = func_value_2
+            lamda2 = a2 + fibonacci_num(n - i + 1) / Fn2 * initial_length
+            x2 = x - lamda2 * x_der
+            y2 = y - lamda2 * y_der
+            func_value_2 = function(x2, y2)
+
+        b1 = b2
+        a1 = a2
+        i += 1
+    return (a1 + b1) / 2
